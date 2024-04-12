0274571015176c784594b53b8aeeb88c8e46c8ba
Damian <LaBancariaDevs@gmail.com>
2024-04-12 10:48:32 -0300

    next config suppressed

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                port: '',
-                pathname: '',
-            },
-            {
-                protocol: 'https',
-                hostname: 'img.freepik.com',
-                port: '',
-                pathname: '',
-            },
-        ],
-    }
+    // images: {
+    //     remotePatterns: [
+    //         {
+    //             protocol: 'https',
+    //             hostname: 'lh3.googleusercontent.com',
+    //             port: '',
+    //             pathname: '',
+    //         },
+    //         {
+    //             protocol: 'https',
+    //             hostname: 'img.freepik.com',
+    //             port: '',
+    //             pathname: '',
+    //         },
+    //     ],
+    // }
 };
 
 export default nextConfig;
